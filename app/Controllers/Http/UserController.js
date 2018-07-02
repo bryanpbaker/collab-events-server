d5874fd0621fabfa4a60fa899a23cf9903126130
Baker <bryan.baker@dal1279-m.nsi.int>
2018-07-02 10:45:34 -0500

    added nodemon, added store() to UserController

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,30 +1,37 @@
 const User = use('App/Models/User');
 
 class UserController {
+  /**
+   * create a new user with required fields
+   * @param {Object} context - destructure request
+   */
   async store({ request }) {
-    console.log(request);
-    // const {
-    //   body: { email, name, username, password, picture }
-    // } = request.all();
+    const { email, name, username, password, picture } = request.body;
 
-    // try {
-    //   const user = await User.findOrCreate(
-    //     { email: email.toLowerCase() },
-    //     {
-    //       email: email.toLowerCase(),
-    //       name,
-    //       username,
-    //       password
-    //     }
-    //   );
+    try {
+      const user = await User.create({
+        email: email.toLowerCase(),
+        name,
+        username,
+        picture,
+        password
+      });
 
-    //   return {
-    //     success: true,
-    //     user
-    //   };
-    // } catch (error) {
-    //   return error;
-    // }
+      return {
+        success: true,
+        user: {
+          email: user.email,
+          name: user.name,
+          username: user.username,
+          picture
+        }
+      };
+    } catch (error) {
+      return {
+        success: false,
+        error
+      };
+    }
   }
 }
 
