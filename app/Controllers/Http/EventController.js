fc240d037565aeb46755a87364760360d9f4615a
Baker <bryan.baker@dal1279-m.local>
2018-07-03 11:58:06 -0500

    added Event store. WIP

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -1,4 +1,4 @@
-'use strict'
+const Event = use('App/Models/Event');
 
 /**
  * Resourceful controller for interacting with events
@@ -8,50 +8,45 @@ class EventController {
    * Show a list of all events.
    * GET events
    */
-  async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new event.
-   * GET events/create
-   */
-  async create ({ request, response, view }) {
-  }
+  async index({ request, response, view }) {}
 
   /**
    * Create/save a new event.
    * POST events
    */
-  async store ({ request, response }) {
+  async store({ request, response, auth }) {
+    const { groupId, name, description } = request.body;
+
+    return Event.create({
+      group_id: groupId,
+      name,
+      description
+    });
   }
 
   /**
    * Display a single event.
    * GET events/:id
    */
-  async show ({ params, request, response, view }) {
-  }
+  async show({ params, request, response, view }) {}
 
   /**
    * Render a form to update an existing event.
    * GET events/:id/edit
    */
-  async edit ({ params, request, response, view }) {
-  }
+  async edit({ params, request, response, view }) {}
 
   /**
    * Update event details.
    * PUT or PATCH events/:id
    */
-  async update ({ params, request, response }) {
-  }
+  async update({ params, request, response }) {}
 
   /**
    * Delete a event with id.
    * DELETE events/:id
    */
-  async destroy ({ params, request, response }) {
-  }
+  async destroy({ params, request, response }) {}
 }
 
-module.exports = EventController
+module.exports = EventController;
